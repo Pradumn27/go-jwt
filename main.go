8820f636e4c4a6b88fb9e90e530a596c31ae847c
Pradumn <pradumn@tealfeed.com>
2025-02-20 14:08:07 +0530

    chore: added mongodb connection and a temp user model

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"go-jwt/config"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config.ConnectDB()
 	app := fiber.New()
 
 	log.Fatal(app.Listen(":3000"))
